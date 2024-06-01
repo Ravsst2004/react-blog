59df235ca83e80f5fdfeb35134c6d07cc80ebe85
Ravsst2004 <ravindraviswanatha@gmail.com>
2024-06-01 19:43:15 +0800

    Revert "Change url pass from blogId to blogTitle"
    This reverts commit 7d74075e50baf2e9b78f675b79507aaba05bb525.

--- src/components/blog/BlogItem.js
+++ src/components/blog/BlogItem.js
@@ -15,9 +15,7 @@ const BlogItem = ({ blogs }) => {
           <p className="">{blog.content}</p>
 
           <Link
-            href={`/detail-blog/${blog.title
-              .replace(/\s+/g, "")
-              .toLowerCase()}`}
+            href={`/detail-blog/${blog.id}`}
             className="max-w-[10rem] text-center p-2 bg-blue-300 border border-black rounded-lg"
           >
             Read More
