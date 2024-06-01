a68af6f2f19d9a8be378e94ef9427a926f35c48e
Ravsst2004 <ravindraviswanatha@gmail.com>
2024-06-01 19:17:56 +0800

    Alert Confirmation delete using Custom Modal Alert

---
+++ src/components/ui/ConfirmModal.js
@@ -0,0 +1,41 @@
+const ConfirmModal = ({ onConfirm, onCancel }) => {
+  return (
+    <div className="fixed inset-0 z-50 flex items-center justify-center backdrop-blur confirm-dialog ">
+      <div className="relative min-h-screen px-4 md:flex md:items-center md:justify-center">
+        <div className="absolute inset-0 z-10 w-full h-full opacity-25 "></div>
+        <div className="fixed inset-x-0 bottom-0 z-50 p-4 mx-4 mb-4 bg-white rounded-lg shadow-lg md:max-w-md md:mx-auto md:relative">
+          <div className="items-center md:flex">
+            <div className="flex items-center justify-center flex-shrink-0 w-16 h-16 mx-auto border border-gray-300 rounded-full">
+              <i className="text-3xl bx bx-error">&#9888;</i>
+            </div>
+            <div className="mt-4 text-center md:mt-0 md:ml-6 md:text-left">
+              <p className="font-bold">Warning!</p>
+              <p className="mt-1 text-sm text-gray-700">
+                You will lose your blog by deleting this. This action cannot be
+                undone.
+              </p>
+            </div>
+          </div>
+          <div className="mt-4 text-center md:text-right md:flex md:justify-end">
+            <button
+              onClick={onConfirm}
+              id="confirm-delete-btn"
+              className="block w-full px-4 py-3 text-sm font-semibold text-red-700 bg-red-200 rounded-lg md:inline-block md:w-auto md:py-2 md:ml-2 md:order-2"
+            >
+              Delete
+            </button>
+            <button
+              onClick={onCancel}
+              id="confirm-cancel-btn"
+              className="block w-full px-4 py-3 mt-4 text-sm font-semibold bg-gray-200 rounded-lg md:inline-block md:w-auto md:py-2 md:mt-0 md:order-1"
+            >
+              Cancel
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ConfirmModal;
